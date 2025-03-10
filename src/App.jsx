00dd433195ff6453b4da6632964ca6101b2de175
ruslansymonenko <symonenkors@gmail.com>
2025-03-10 18:53:08 +0200

    working on app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Collections from './pages/Collections.jsx';
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
+import Product from './pages/Product.jsx';
+import Cart from './pages/Cart.jsx';
+import Login from './pages/Login.jsx';
+import PlaceOrder from './pages/PlaceOrder.jsx';
+import Orders from './pages/Orders.jsx';
+import Navbar from './components/Navbar.jsx';
 
 const App = () => {
   return (
-      <div>
-        Hello
-      </div>
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collections" element={<Collections />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
+    </div>
   );
 };
 
